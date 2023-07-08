9395f89a8a82ed3598e86cce846e3a5c877b4155
hilmi saftian <hilmi@esb.co.id>
2023-07-08 23:51:37 +0700

    first commit

---
+++ api/app/Http/Controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+    
+    public function user(Request $request)
+    {   
+        return $request->user('api');
+    }
+    
+    public function register(UserRegisterRequest $request)
+    {
+        User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        
+        return 'user created successfully';
+    }
+}
